d2c7b823295ea7af8d7678d494c742b1bb66889a
Xuanphat1754 <3203500@visioncollege.ac.nz>
2026-09-09 12:16:44 +1200

    change the last port for backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://localhost:5001',
         secure: false,
       },
     },
